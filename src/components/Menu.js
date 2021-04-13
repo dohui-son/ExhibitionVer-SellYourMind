afe57713e8ec08a3a00b6fc436ff2381f90779b2
dohuison <thsehgml37@korea.ac.kr>
2021-04-13 23:19:34 +0900

    mobile menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Link } from "react-router-dom";
 import menu from "../stylesheet/menu.scss";
+import MobileMenuList from "./MobileMenuList";
 
 const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const close = () => {
+    setIsOpen(false);
+  };
   return (
     <div className="menu-overlay">
       <div className="menus">
-      <Link to="/">Home</Link>
-      <Link to="/auth">LogIn</Link>
-      <Link to="/mypage">MyPage</Link>
-      <Link to="/market">Market</Link>
-      <Link to="/sell">Sell Your Mind</Link>
-      <span>메뉴</span>
+        <Link to="/">Home</Link>
+        <Link to="/auth">LogIn</Link>
+        <Link to="/mypage">MyPage</Link>
+        <Link to="/market">Market</Link>
+        <Link to="/sell">Sell Your Mind</Link>
       </div>
+      <div className="menu-button" onClick={setIsOpen}>
+        <div className="line" />
+        <div className="line" />
+        <div className="line" />
+      </div>
+      <MobileMenuList isOpen={isOpen} close={close} />
     </div>
   );
 };
---
+++ src/components/MobileMenuList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "../stylesheet/menu.scss";
+
+const MobileMenuList = ({ isOpen, close }) => {
+  return (
+    <React.Fragment>
+      {isOpen ? (
+        <React.Fragment>
+          <div className="menuOverlay" onClick={close} />
+          <div className="menu">
+            <div className="content">
+              <Link to="/">Home</Link>
+              <Link to="/auth">LogIn</Link>
+              <Link to="/mypage">MyPage</Link>
+              <Link to="/market">Market</Link>
+              <Link to="/sell">Sell Your Mind</Link>
+            </div>
+            <div className="buttonWrap">
+              <button onClick={close}>close</button>
+            </div>
+          </div>
+        </React.Fragment>
+      ) : null}
+    </React.Fragment>
+  );
+};
+export default MobileMenuList;
