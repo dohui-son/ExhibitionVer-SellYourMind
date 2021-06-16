b4fa769afe69dcfc58339fd91e77faea8d7762e0
dohui-son <thsehgml37@korea.ac.kr>
2021-06-17 03:54:41 +0900

    chore : link to 3d

--- src/page/Market.js
+++ src/page/Market.js
@@ -47,170 +47,289 @@ const Market = ({ isVisible, noheader, onheader }) => {
           </div>
           <div className="line" />
           <div className="contents">
-            누적수령 마스크<div className="bolded">5 개</div>
+            누적수령 마스크
+            <div className="bolded">
+              <Link to="/d7">035</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            재활용한 마스크<div className="bolded">총29개</div>
+            재활용한 마스크
+            <div className="bolded">
+              <Link to="/d42">222</Link>
+            </div>
           </div>
           <div className="line" />
 
           <div className="contents">
-            재활용level<div className="bolded">새싹단계</div>
+            재활용level
+            <div className="bolded">
+              <Link to="/d36">005</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d43">001</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d21">021</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/sell/d39">000</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d38">003</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d31">010</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d37">004</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              {' '}
+              <Link to="/d4">038</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d34">007</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d20">022</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d30">011</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d29">012</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d28">013</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d27">014</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d41">000</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d26">015</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d25">016</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d24">017</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d23">018</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d22">020</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d18">024</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d17">025</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d9">033</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d33">008</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d16">026</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d15">027</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d14">028</Link>
+            </div>
           </div>
           <div className="line" />
           <div className="contents">
-            부족 포인트<div className="bolded">2 point</div>
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d13">029</Link>
+            </div>
+          </div>
+
+          <div className="line" />
+          <div className="contents">
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d12">030</Link>
+            </div>
+          </div>
+          <div className="line" />
+          <div className="contents">
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d11">031</Link>
+            </div>
+          </div>
+          <div className="line" />
+          <div className="contents">
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d40">002</Link>
+            </div>
+          </div>
+          <div className="line" />
+          <div className="contents">
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d10">032</Link>
+            </div>
+          </div>
+          <div className="line" />
+          <div className="contents">
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d8">034</Link>
+            </div>
+          </div>
+          <div className="line" />
+          <div className="contents">
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d6">036</Link>
+            </div>
+          </div>
+
+          <div className="line" />
+          <div className="contents">
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d5">037</Link>
+            </div>
+          </div>
+          <div className="line" />
+          <div className="contents">
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d3">어린왕자</Link>
+            </div>
+          </div>
+          <div className="line" />
+          <div className="contents">
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d2">가장 슬펐던 날</Link>
+            </div>
+          </div>
+          <div className="line" />
+          <div className="contents">
+            부족 포인트
+            <div className="bolded">
+              <Link to="/d1">지구의 질감</Link>
+            </div>
           </div>
         </div>
-
-        <Link to="/d19">023</Link>
-        <Link to="/d7">035</Link>
-        <Link to="/d42">222</Link>
-        <Link to="/d36">005</Link>
-        <Link to="/d43">001</Link>
-
-        <Link to="/d21">021</Link>
-
-        <Link to="/sell/d39">000</Link>
-        <Link to="/d38">003</Link>
-        <Link to="/d31">010</Link>
-        <Link to="/d37">004</Link>
-        <Link to="/d4">038</Link>
-        <Link to="/d34">007</Link>
-        <Link to="/d20">022</Link>
-        <Link to="/d30">011</Link>
-        <Link to="/d29">012</Link>
-        <Link to="/d28">013</Link>
-        <Link to="/d27">014</Link>
-        <Link to="/d41">000</Link>
-        <Link to="/d26">015</Link>
-        <Link to="/d25">016</Link>
-        <Link to="/d24">017</Link>
-        <Link to="/d23">018</Link>
-        <Link to="/d22">020</Link>
-        <Link to="/d18">024</Link>
-        <Link to="/d17">025</Link>
-        <Link to="/d9">033</Link>
-        <Link to="/d33">008</Link>
-        <Link to="/d16">026</Link>
-        <Link to="/d15">027</Link>
-        <Link to="/d14">028</Link>
-        <Link to="/d13">029</Link>
-        <Link to="/d12">030</Link>
-        <Link to="/d11">031</Link>
-        <Link to="/d40">002</Link>
-        <Link to="/d10">032</Link>
-        <Link to="/d8">034</Link>
-        <Link to="/d6">036</Link>
-        <Link to="/d5">037</Link>
-        <Link to="/d3">어린왕자</Link>
-        <Link to="/d2">가장 슬펐던 날</Link>
-        <Link to="/d1">지구의 질감</Link>
 
         <div className="footer">
           <p>whole project by ARTIST DOHUI SON .</p>
