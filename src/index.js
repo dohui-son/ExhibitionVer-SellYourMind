53f3ca58864eee2c409a2599e233aecef078ee1c
dohui-son <thsehgml37@korea.ac.kr>
2021-06-17 02:43:47 +0900

    chore : hashrouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import Route, { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Route, { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </HashRouter>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
